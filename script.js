08e0301065501f4a92894fa603845d13b42e3ea6
ruimerquidi-coder <ruimerquidi@gmail.com>
2026-05-24 19:25:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+window.addEventListener("scroll", () => {
 
+    const scroll = window.scrollY;
+
+    document.querySelector(".background").style.transform =
+    `translateY(${scroll * 0.2}px)`;
+
+});
